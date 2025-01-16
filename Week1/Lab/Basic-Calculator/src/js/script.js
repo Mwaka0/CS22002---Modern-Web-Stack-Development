b104f4cbec1e28dcba461037a40516e93fea768a
Dylan Mwakasekele <Dylanmwakasekele@gmail.com>
2025-01-16 15:38:08 +0000

    Added reset button

--- Week1/Lab/Basic-Calculator/src/js/script.js
+++ Week1/Lab/Basic-Calculator/src/js/script.js
@@ -1,3 +1,7 @@
+/**
+ * A function that gets the numbers from the input fields
+ * @returns num1 and num2
+ */
 function getNumbers() {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
@@ -7,16 +11,23 @@ function getNumbers() {
 /**
  * A function that calculates the result of the operation based on the operation type
  * @param {*} operation 
+ * @returns the result of the operation
  */
 function calculate(operation) {
     // Get the numbers from the input fields
     const {num1, num2} = getNumbers();
-    var result = 0;
+    let result = 0;
 
     // Check if the input is a number
     if (isNaN(num1) || isNaN(num2)) {
         console.log("Invalid input, please enter a valid number");
-        return;
+        return "Error, please enter valid numbers in both fields.";
+    }
+
+    // Check if the operation is divide and the second number is zero
+    if (operation === 'divide' && num2 === 0) {
+        console.log("Cannot divide by zero...");
+        return "Error, Cannot divide by zero.";
     }
     
     // Perform the operation based on the operation type
@@ -36,9 +47,36 @@ function calculate(operation) {
         default:
             // If the operation is not valid, print an error message
             console.log("Invalid operation..."); 
-            break;
+            return "Error, Invalid operation.";
     }
 
     // Display the result in the result field
     return result;
 }
+
+function reset() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").textContent = "";
+}
+
+window.onload = function() {
+    document.getElementById('add').addEventListener('click', () => {
+        document.getElementById('result').textContent = `${calculate('add')}`;
+    });
+
+    document.getElementById('subtract').addEventListener('click', () => {
+        document.getElementById('result').textContent = `${calculate('subtract')}`;
+    });
+
+    document.getElementById('multiply').addEventListener('click', () => {
+        document.getElementById('result').textContent = `${calculate('multiply')}`;
+    });
+
+    document.getElementById('divide').addEventListener('click', () => {
+        document.getElementById('result').textContent = `${calculate('divide')}`;
+    });
+    document.getElementById('reset').addEventListener('click', () => {
+        reset();
+    });
+};
